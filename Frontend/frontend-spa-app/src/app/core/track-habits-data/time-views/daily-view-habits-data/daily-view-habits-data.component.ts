3efc9f09be33b87ef61d9c2dcb025f2e951ee3e4
woroniecki <dwrona94@gmail.com>
2025-04-10 13:58:09 +0200

    Created habit card

--- Frontend/frontend-spa-app/src/app/core/track-habits-data/time-views/daily-view-habits-data/daily-view-habits-data.component.ts
+++ Frontend/frontend-spa-app/src/app/core/track-habits-data/time-views/daily-view-habits-data/daily-view-habits-data.component.ts
@@ -2,15 +2,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { GetHabitsDataQueryResponse } from '../../../../api-client/models/get-habits-data-query-response';
 import { CommonModule } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-daily-view-habits-data',
-  imports: [CommonModule, AngularSvgIconModule],
+  imports: [CommonModule, AngularSvgIconModule, DragDropModule],
   templateUrl: './daily-view-habits-data.component.html',
   styleUrl: './daily-view-habits-data.component.css',
   standalone: true,
 })
 export class DailyViewHabitsDataComponent {
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
+
   @Input() items: Array<GetHabitsDataQueryResponse> = [];
   @Input() date: Date = new Date();
   @Output() onClickHabit = new EventEmitter<{ habitId: string; date: Date }>();
