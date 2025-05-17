d605035ef3845ba2d5e0b0ec8d314ff916db8123
woroniecki <dwrona94@gmail.com>
2025-05-17 15:54:37 +0200

    Show only gmail sign in btn

--- Frontend/frontend-spa-app/src/app/landing-page/landing-page.component.ts
+++ Frontend/frontend-spa-app/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../services/auth.service';
@@ -7,6 +7,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { AuthGoogleService } from '../services/auth-google.service';
 import { RegisterComponent } from '../register/register.component';
 import { LoginComponent } from '../login/login.component';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-landing-page',
@@ -15,12 +16,18 @@ import { LoginComponent } from '../login/login.component';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  isProduction: boolean = false;
+
   constructor(
     private dialog: MatDialog,
     public auth: AuthService,
     public authGoogle: AuthGoogleService,
   ) {}
+
+  ngOnInit(): void {
+    this.isProduction = environment.production;
+  }
 
   onRegister() {
     this.dialog.open(RegisterComponent, {
